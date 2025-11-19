7a68422b1f405762bbfa36d7de950ad67ad41ab5
DAlmasan <100142108+DAlmasan@users.noreply.github.com>
2025-11-19 17:56:43 +0200

    Update CalcsTest.java

--- src/test/java/CalcsTest.java
+++ src/test/java/CalcsTest.java
@@ -2,33 +2,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalcsTest {
+
     @Test
-    public void alwaysFail()
-    {
-        Assert.assertEquals(1, 2);
+    public void alwaysFail() {
+        assertEquals(2, 1); // swapped order to follow assertEquals(expected, actual)
     }
+
     @Test
-    public void alwaysFail1()
-    {
-        Assert.assertEquals(1, 2);
+    public void alwaysFail1() {
+        assertEquals(2, 1);
     }
+
     @Test
-    public void alwaysFail2()
-    {
-        Assert.assertEquals(1, 2);
+    public void alwaysFail2() {
+        assertEquals(2, 1);
     }
-     @Test
-    public void ESInProgress()
-    {
-        Assert.assertEquals(1, 3);
-    }
-      @Test
-    public void testestestestest()
-    {
-        Assert.assertEquals(1, 1);
-    }
+
     @Test
-	public void sum_test_A() {
-	Assert.assertEquals(3, VerifyDefect.sum(1,2));
-	}
-}
+    public void ESInProgress() {
+        assertEquals(3, 1);
+    }
+
+    @Test
+    public void testestestestest() {

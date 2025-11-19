1aa94df5aed462c641b4aa33bea9b1fcede4240e
DAlmasan <100142108+DAlmasan@users.noreply.github.com>
2025-11-19 17:36:58 +0200

    Update TheseModifiedNameTest.java

--- src/test/java/TheseModifiedNameTest.java
+++ src/test/java/TheseModifiedNameTest.java
@@ -1,200 +1,174 @@
-//import junit.framework.Assert;
-
-import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 
 import java.util.Random;
 
-                /**  
-                 * Created by dshmaya on 15/11/2017 ...04
-                */  
-                public class TheseModifiedNameTest{
-                    private int a = 1;
-                    private int b =1;
-                 
-                                
-                   @Test  
-                   public void alwaysPass1()
-                   {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-                       Assert.assertEquals(a, 1);
-                       Assert.assertEquals(1, 2);
-                   }
-			
-		
-		   @Test  
-                   public void alwaysPass1New()
-                   {
-                    
-                       Assert.assertEquals(1, 5);
-                   }
-			
-		   @Test  
-                   public void alwaysPass1New1()
-                   {
-                    
-                       Assert.assertEquals(1, 2);
-                   }
-                
-		   @Test
-		   public void alwaysPass1New12()
-                   {
-                    
-                       Assert.assertEquals(1, 1);
-                   }
-                   @Test
-                    public void alwaysPass2()
-                    {
-                        Assert.assertEquals(1, 2);
-                        System.out.println("proba9");
-                    }
-                   @Test  
-                   public void alwaysPass3()
-                   {
-                       Assert.assertEquals(1, 1);
-                   }
+class TheseModifiedNameTest {
 
-                    @Test
-                    public void alwaysPassD()
-                    {
-                        Assert.assertEquals(1, 1);
-                    }
+    private int a = 1;
+    private int b = 1;
 
-                    @Test
-                    public void alwaysPass4()
-                    {
-                        Assert.assertEquals(1, 1);
-                    }
-                    @Test
-                    public void alwaysPassA()
-                    {
-                        Assert.assertEquals(1, 1);
-                    }
-                    @Test
-                    public void alwaysPassB()
-                    {
-                        Assert.assertEquals(1, 2);
-                    }
-                    @Test
-                    public void alwaysPassG()
-                    {
-                        Assert.assertEquals(1, 1);
-                        System.out.println("proba7");
-
-
-                    }
-                    @Test
-                    public void alwaysPassH()
-                    {
-                        Assert.assertEquals(1, 2);
-                        System.out.println("proba8kodojoso");
-
-
-                    }
-                    @Test
-                    public void alwaysPassF()
-                    {
-                        Assert.assertEquals(1, 2);
-                    }
-                   @Test
-                   public void alwaysFail5()
-                   {
-                       Assert.assertEquals(1, 1);
-                   }
-
-
-                  @Test
-                    public void alwaysPass6()
-                    {
-                        Assert.assertEquals(1, 2);
-                    }
-                    @Test
-                    public void alwaysPassC()
-                    {
-                        Assert.assertEquals(1, 5);
-                    }
-                  @Test
-                    public void alwaysPass7()
-                    {
-                        Assert.assertEquals(1, 1);
-                    }
-                 @Test
-                   public void alwaysFail81()
-                   {
-                       Assert.assertEquals(1, 1);
-                   }
-                  @Test
-                   public void alwaysFail91()
-                   {
-                       Assert.assertEquals(1, 1);
-                   }
-                  @Test
-                   public void alwaysFail10()
-                   {
-                       Assert.assertEquals(1, 1);
-                       System.out.println("Neata!");
-                   }
-                  @Test
-                   public void alwaysFail11()
-                   {
-                       Assert.assertEquals(1, 1);
-                   }
-                  @Test
-                   public void alwaysFail12()
-                   {
-                       Assert.assertEquals(1, 1);
-                   }
-                  @Test
-                   public void alwaysFail13()
-                   {
-                       Assert.assertEquals(1, 1);
-                   }
-                    @Test
-                    public void alwaysFailE()
-                    {
-                        Assert.assertEquals(1, 1);
-                        System.out.println("Neata2!");
-                    }
-                  
-                    @Test
-                    @Ignore
-                    public void ignored1()
-                    {
-                        Random random = new Random();
-                        int randomNumber = random.nextInt(99);
-                        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-                        Assert.assertEquals(smallerThanFiftee, true);            
-                    }
-                                
-                   @Test
-                   @Ignore
-                    public void ignored2()
-                    {
-                        Random random = new Random();
-                        int randomNumber = random.nextInt(100);
-                        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-                        Assert.assertEquals(smallerThanFiftee, true);
-                    }
-			@Ignore
-    @Test	
-    public void sometimesPass888200()
-    {
-
-         Random random = new Random();
-         int randomNumber = random.nextInt(100);
-         boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-         Assert.assertEquals(smallerThanFiftee, true);
-        
-    }
-	@Ignore		
     @Test
-    public void sometimesPass888198()
-    {
+    void alwaysPass1() {
+        assertEquals(a, 1);
+        assertEquals(1, 2);
+    }
 
-         Random random = new Random();
-         int randomNumber = random.nextInt(100);
-         boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-         Assert.assertEquals(smallerThanFiftee, true);
-     }
+    @Test
+    void alwaysPass1New() {
+        assertEquals(1, 5);
+    }
+
+    @Test
+    void alwaysPass1New1() {
+        assertEquals(1, 2);
+    }
+
+    @Test
+    void alwaysPass1New12() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysPass2() {
+        assertEquals(1, 2);
+        System.out.println("proba9");
+    }
+
+    @Test
+    void alwaysPass3() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysPassD() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysPass4() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysPassA() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysPassB() {
+        assertEquals(1, 2);
+    }
+
+    @Test
+    void alwaysPassG() {
+        assertEquals(1, 1);
+        System.out.println("proba7");
+    }
+
+    @Test
+    void alwaysPassH() {
+        assertEquals(1, 2);
+        System.out.println("proba8kodojoso");
+    }
+
+    @Test
+    void alwaysPassF() {
+        assertEquals(1, 2);
+    }
+
+    @Test
+    void alwaysFail5() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysPass6() {
+        assertEquals(1, 2);
+    }
+
+    @Test
+    void alwaysPassC() {
+        assertEquals(1, 5);
+    }
+
+    @Test
+    void alwaysPass7() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysFail81() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysFail91() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysFail10() {
+        assertEquals(1, 1);
+        System.out.println("Neata!");
+    }
+
+    @Test
+    void alwaysFail11() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysFail12() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysFail13() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void alwaysFailE() {
+        assertEquals(1, 1);
+        System.out.println("Neata2!");
+    }
+
+    @Disabled
+    @Test
+    void ignored1() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(99);
+        boolean smallerThanFiftee = randomNumber < 50;
+        assertEquals(true, smallerThanFiftee);
+    }
+
+    @Disabled
+    @Test
+    void ignored2() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        boolean smallerThanFiftee = randomNumber < 50;
+        assertEquals(true, smallerThanFiftee);
+    }
+
+    @Disabled
+    @Test
+    void sometimesPass888200() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        boolean smallerThanFiftee = randomNumber < 50;
+        assertEquals(true, smallerThanFiftee);
+    }
+
+    @Disabled
+    @Test
+    void sometimesPass888198() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        boolean smallerThanFiftee = randomNumber < 50;
+        assertEquals(true, smallerThanFiftee);
+    }
 }
